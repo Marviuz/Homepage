51ecbab6ff8a7be82373162f74eb0373b38bc198
Marviuz <marcusleviticusd@gmail.com>
2019-07-03 14:59:31 +0800

    Added change opacity Added reload button on `No network`

--- src/App.vue
+++ src/App.vue
@@ -3,182 +3,219 @@
     :style="{ backgroundColor: theme.primary, backgroundImage: siteBg ? `url(${siteBg})` : null }"
     :class="`${siteBg ? 'app-custom': null}`"
   >
-    <v-toolbar
-      app
-      :color="lightenOrDarken(theme.primary)"
-    >
-      <v-toolbar-title>
-        <v-icon
-          class="mr-2"
-          :style="{ color: theme.text }"
-        >
-          home
-        </v-icon>
-        <span :style="{ color: theme.text }">I'm Home!</span>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-toolbar-items v-if="$firebase.auth().currentUser">
-        <v-menu
-          offset-y
-          :close-on-content-click="false"
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn
-              flat
-              v-on="on"
-            >
-              <v-avatar
-                :size="40"
-                class="mr-2"
-              >
-                <img
-                  :src="$firebase.auth().currentUser.photoURL"
-                  :alt="$firebase.auth().currentUser"
-                >
-              </v-avatar>
-              <span :style="{ color: theme.text }">
-                {{ $firebase.auth().currentUser.displayName }}
-              </span>
-            </v-btn>
-          </template>
-          <v-list>
-            <v-list-tile @click="changeColor.dialog = true">
-              <v-list-tile-content>
-                <v-list-tile-title>Change color</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile @click="logoutFromGoogle">
-              <v-list-tile-content>
-                <v-list-tile-title>Logout</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-menu>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-content>
-      <v-snackbar
-        color="error"
-        :value="!isConnected['.value']"
-        :timeout="0"
-        top
+    <div :style="{ opacity: (opacity / 100).toFixed(2) }">
+      <v-toolbar
+        app
+        :color="lightenOrDarken(theme.primary)"
       >
-        <v-layout
-          align-center
-          grid-list-lg
-        >
-          <v-flex shrink>
-            <v-icon color="error darken-4">
-              warning
-            </v-icon>
-          </v-flex>
-          <v-flex>
-            <span class="headline pl-2">
-              No network!
-            </span>
-          </v-flex>
-        </v-layout>
-      </v-snackbar>
-
-      <v-snackbar
-        color="error"
-        :value="saveError.value"
-        top
-      >
-        <v-layout
-          align-center
-          grid-list-lg
-        >
-          <v-flex shrink>
-            <v-icon color="error darken-4">
-              warning
-            </v-icon>
-          </v-flex>
-          <v-flex>
-            <span class="headline pl-2">
-              {{ saveError.msg }}
-            </span>
-          </v-flex>
-        </v-layout>
-      </v-snackbar>
-
-      <v-snackbar
-        :value="isSaved"
-        top
-      >
-        <div class="font-weight-thin headline">
-          Saved changes!
-        </div>
-      </v-snackbar>
-      <router-view />
-    </v-content>
-    <v-dialog
-      v-model="changeColor.dialog"
-      persistent
-      width="500"
-    >
-      <v-card>
-        <v-form @submit.prevent="handleChangeColor">
-          <v-toolbar
-            :color="theme.primary"
-            flat
+        <v-toolbar-title>
+          <v-icon
+            class="mr-2"
+            :style="{ color: theme.text }"
           >
-            <v-card-title
-              class="title"
-              :style="{ color: theme.text }"
-            >
-              Customize
-            </v-card-title>
-            <v-spacer />
-            <v-btn
-              icon
-              :color="theme.text"
-              flat
-              @click="handleColorReset"
-            >
-              <v-icon>close</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <v-container grid-list-lg>
-            <v-layout grid-list-lg>
-              <v-flex xs8>
-                <v-layout column>
-                  <v-flex grow>
-                    <m-color-picker
-                      v-model="newTheme.primary"
-                      label="Background"
-                    />
-                  </v-flex>
-                  <v-flex grow>
-                    <m-color-picker
-                      v-model="newTheme.text"
-                      label="Text"
-                    />
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-              <v-flex
-                xs4
-                :style="{ pointerEvents: 'none', userSelect: 'none' }"
+            home
+          </v-icon>
+          <span :style="{ color: theme.text }">I'm Home!</span>
+        </v-toolbar-title>
+        <v-spacer />
+        <v-toolbar-items v-if="$firebase.auth().currentUser">
+          <v-menu
+            offset-y
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                flat
+                v-on="on"
               >
-                <m-site-preview v-model="newTheme" />
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              depressed
-              type="submit"
-              :color="theme.primary || 'primary'"
-              :style="{ color: theme.text }"
+                <v-avatar
+                  :size="40"
+                  class="mr-2"
+                >
+                  <img
+                    :src="$firebase.auth().currentUser.photoURL"
+                    :alt="$firebase.auth().currentUser"
+                  >
+                </v-avatar>
+                <span :style="{ color: theme.text }">
+                  {{ $firebase.auth().currentUser.displayName }}
+                </span>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-tile @click="changeColor.dialog = true">
+                <v-list-tile-content>
+                  <v-list-tile-title>Change color</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-action>
+                  <v-slider
+                    v-model="opacity"
+                    thumb-label
+                    :color="theme.primary"
+                    width="100%"
+                    :max="100"
+                    :min="0"
+                  >
+                    <template v-slot:thumb-label="props">
+                      <span>
+                        {{ props.value }}%
+                      </span>
+                    </template>
+                  </v-slider>
+                </v-list-tile-action>
+                <v-list-tile-action v-if="theme.opacity !== opacity">
+                  <v-btn
+                    class="ml-auto"
+                    flat
+                    icon
+                    :color="theme.primary"
+                    @click="saveOpacity"
+                  >
+                    <v-icon>save</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-list-tile @click="logoutFromGoogle">
+                <v-list-tile-content>
+                  <v-list-tile-title>Logout</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-content>
+        <v-snackbar
+          color="error"
+          :value="!isConnected['.value']"
+          :timeout="0"
+          top
+        >
+          <v-layout
+            align-center
+            grid-list-lg
+          >
+            <v-flex shrink>
+              <v-icon color="error darken-4">
+                warning
+              </v-icon>
+            </v-flex>
+            <v-flex>
+              <span class="headline pl-2">
+                No network!
+              </span>
+            </v-flex>
+          </v-layout>
+          <v-btn
+            icon
+            @click="$router.go()"
+          >
+            <v-icon>refresh</v-icon>
+          </v-btn>
+        </v-snackbar>
+
+        <v-snackbar
+          color="error"
+          :value="saveError.value"
+          top
+        >
+          <v-layout
+            align-center
+            grid-list-lg
+          >
+            <v-flex shrink>
+              <v-icon color="error darken-4">
+                warning
+              </v-icon>
+            </v-flex>
+            <v-flex>
+              <span class="headline pl-2">
+                {{ saveError.msg }}
+              </span>
+            </v-flex>
+          </v-layout>
+        </v-snackbar>
+
+        <v-snackbar
+          :value="isSaved"
+          top
+        >
+          <div class="font-weight-thin headline">
+            Saved changes!
+          </div>
+        </v-snackbar>
+        <router-view />
+      </v-content>
+      <v-dialog
+        v-model="changeColor.dialog"
+        persistent
+        width="500"
+      >
+        <v-card>
+          <v-form @submit.prevent="handleChangeColor">
+            <v-toolbar
+              :color="theme.primary"
+              flat
             >
-              Save
-            </v-btn>
-          </v-card-actions>
-        </v-form>
-      </v-card>
-    </v-dialog>
+              <v-card-title
+                class="title"
+                :style="{ color: theme.text }"
+              >
+                Customize
+              </v-card-title>
+              <v-spacer />
+              <v-btn
+                icon
+                :color="theme.text"
+                flat
+                @click="handleColorReset"
+              >
+                <v-icon>close</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-container grid-list-lg>
+              <v-layout grid-list-lg>
+                <v-flex xs8>
+                  <v-layout column>
+                    <v-flex grow>
+                      <m-color-picker
+                        v-model="newTheme.primary"
+                        label="Background"
+                      />
+                    </v-flex>
+                    <v-flex grow>
+                      <m-color-picker
+                        v-model="newTheme.text"
+                        label="Text"
+                      />
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+                <v-flex
+                  xs4
+                  :style="{ pointerEvents: 'none', userSelect: 'none' }"
+                >
+                  <m-site-preview v-model="newTheme" />
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                depressed
+                type="submit"
+                :color="theme.primary || 'primary'"
+                :style="{ color: theme.text }"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-dialog>
+    </div>
   </v-app>
 </template>
 
@@ -193,6 +230,8 @@ export default {
         primary: '#000000',
         text: '#000000',
       },
+
+      opacity: 0,
 
       isSaved: false,
       saveError: {
@@ -221,13 +260,31 @@ export default {
       isConnected: this.$database('.info/connected'),
     };
   },
+  watch: {
+    'theme.opacity': {
+      immediate: true,
+      handler(val) {
+        if (val) this.opacity = val;
+        else this.opacity = 100;
+      },
+    },
+  },
   methods: {
+    saveOpacity() {
+      this.$database(`${this.$firebase.auth().currentUser.uid}/theme`)
+        .update({ opacity: this.opacity })
+        .then(() => {})
+        .catch((err) => {
+          this.saveError = {
+            value: true,
+            msg: err.message,
+          };
+        });
+    },
     handleChangeColor() {
       this.$database(`${this.$firebase.auth().currentUser.uid}/theme`)
         .update(this.newTheme)
-        .then(() => {
-          this.isSaved = true;
-        })
+        .then(() => {})
         .catch((err) => {
           this.saveError = {
             value: true,
@@ -264,9 +321,5 @@ export default {
   background-size: cover !important;
   background-repeat: no-repeat !important;
   background-attachment: fixed !important;
-
-  & > * {
-    opacity: .95;
-  }
 }
 </style>
